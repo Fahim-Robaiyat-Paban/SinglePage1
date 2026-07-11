094e5042152f0ae38c73d1aedcd26f456aad671f
Fahim-Robaiyat-Paban <fahimrobaiyatpaban@gmail.com>
2026-07-11 15:17:18 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './componenets/Navbar'
 import Banner from './componenets/Banner'
+import Corewave from './componenets/Corewave'
 
 const App = () => {
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Corewave></Corewave>
     </>
   )
 }
--- src/componenets/Banner.jsx
+++ src/componenets/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
                         <button className="explore-btn">
                         <a className='font-Lato font-[16px] text-[#ffffff] bg-[#06C279] py-3.75 px-6.25 rounded-[5px] mr-5' href="">Explore More</a>
                         </button>
-                        <button className="watch-now-btn border-2 py-[12px] px-6 border-[#06C279] bg-[#EEFFF9] rounded-[5px] flex justify-baseline align-baseline">
+                        <button className="watch-now-btn border-2 py-3 px-6 border-[#06C279] bg-[#EEFFF9] rounded-[5px] flex justify-baseline align-baseline">
                           <img className='w-5 h-5 mr-3' src="public\playbtn.png" alt="" /><a className='font-Lato font-semibold text-[#1B1B1B]' href="">Watch Now</a>
                         </button>
                     </div>

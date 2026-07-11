f251134eaa5f41de557317c70f675a4f14b3d135
Fahim-Robaiyat-Paban <fahimrobaiyatpaban@gmail.com>
2026-07-11 16:35:53 +0600

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './componenets/Navbar'
 import Banner from './componenets/Banner'
 import Corewave from './componenets/Corewave'
+import Services from './componenets/Services'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar></Navbar>
       <Banner></Banner>
       <Corewave></Corewave>
+      <Services></Services>
     </>
   )
 }
